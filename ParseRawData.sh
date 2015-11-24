214ec5a5621b7c4ab85212ddfda5b2b5b1a3f8ca
yoshi <yoshijava@gmail.com>
2015-11-24 10:00:45 +0800

    headers for script files.

--- ParseRawData.sh
+++ ParseRawData.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 # ParseRawData.sh 2015 11 20
 grep -a MTX src/main/resources/input/Daily_$1_$2_$3_C.rpt | tr "," " " | awk '{print $1" "$4" "$7}' | sort -n > src/main/resources/input/$1$2$3_MTX.txt
