89373ec8e19d3e268a3862adf1233531b479f0fb
yoshi <yoshijava@gmail.com>
2015-12-01 03:06:36 +0800

    fixed bugs in word doc. successfully query the unsettled future trade.

--- src/main/java/oracle/sinopac/IT4.java
+++ src/main/java/oracle/sinopac/IT4.java
@@ -3,7 +3,7 @@ import com.sun.jna.win32.*;
 import com.sun.jna.*;
 import com.sun.jna.platform.win32.*;
 
-public interface IT4 extends Library {
+public interface IT4 extends StdCallLibrary {
     public abstract Pointer init_t4(Pointer id, Pointer password, Pointer path);
     public abstract Pointer show_list();
     public abstract Pointer show_ip();
@@ -12,4 +12,7 @@ public interface IT4 extends Library {
         Pointer gubn, Pointer group_name, Pointer branch, Pointer account, Pointer type_1, Pointer type_2, Pointer time_out);
     public abstract Pointer get_response_log();
     public abstract int check_response_buffer();
+    public abstract Pointer add_acc_ca(Pointer branch, Pointer account, Pointer id, Pointer caPath, Pointer password);
+    public abstract Pointer verify_ca_pass(Pointer branch, Pointer account); 
+
 }
--- src/main/java/oracle/sinopac/TestT4.java
+++ src/main/java/oracle/sinopac/TestT4.java
@@ -2,6 +2,7 @@ package oracle.sinopac;
 import com.sun.jna.*;
 import com.sun.jna.platform.win32.*;
 import java.io.*;
+import java.util.*;
 
 public class TestT4 {
     // static {
@@ -13,6 +14,9 @@ public class TestT4 {
     //     }
     // }
 
+    private static String libPath = System.getProperty("user.dir")+"\\libs\\Sinopac\\t4.dll";
+    private static IT4 t4 = (IT4) Native.loadLibrary(libPath, IT4.class);;
+
     public static Pointer toNativeAscii(String myString) {
         Pointer m = new Memory(myString.length() + 1); // WARNING: assumes ascii-only string
         m.setString(0, myString);
@@ -20,16 +24,22 @@ public class TestT4 {
     }
 
     public static String toJString(Pointer pointer) {
-        byte[] array = new byte[256];
+        Vector<Byte> byteVector = new Vector<Byte>();
+        byte b = '\0';
         int i = 0;
-        byte c = '\0';
-        while( (c = pointer.getByte(i)) != '\0' ) {
-            array[i] = c;
+        while( (b = pointer.getByte(i)) != '\0' ) {
+            byteVector.add(b);
             i++;
         }
         String s = null;
+        byte[] bytes = new byte[byteVector.size()];
+        i = 0;
+        for(Byte by : byteVector) {
+            bytes[i] = by.byteValue();
+            i++;
+        }
         try {
-            s = new String(array, "Big5");
+            s = new String(bytes, "Big5");
         }
         catch(UnsupportedEncodingException e) {
             e.printStackTrace();
@@ -37,30 +47,56 @@ public class TestT4 {
         return s;
     }
 
+    private static Vector queryUnsettled(Pointer pBranch, Pointer pAccount) {
+        String[] rets = toJString(
+            t4.fo_unsettled_qry(
+                toNativeAscii("0000"),
+                toNativeAscii("0004"),
+                toNativeAscii("0000"),
+                toNativeAscii("0000"),
+                toNativeAscii("0"),
+                toNativeAscii(""),
+                pBranch,
+                pAccount,
+                toNativeAscii("1"),
+                toNativeAscii("0"),
+                toNativeAscii("1")
+            )).split("\\s");
+        Vector<String> results = new Vector<String>();
+        for(String s : rets) {
+            if(s.isEmpty()==false) {
+                results.add(s);
+            }
+        }
+        return results;
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new FileReader("account.ini"));
         String id = br.readLine();
         String password = br.readLine();
         String branch = br.readLine();
-        String libPath = System.getProperty("user.dir")+"\\libs\\Sinopac\\t4.dll";
-        IT4 t4 = (IT4) Native.loadLibrary(libPath, IT4.class);
+        String account = br.readLine();
+
+
         Pointer pId = toNativeAscii(id);
         Pointer pPassword = toNativeAscii(password);
         Pointer pEmpty = toNativeAscii("");
+        Pointer pAccount = toNativeAscii(account);
+        Pointer pLibPath = toNativeAscii(libPath);
+        Pointer pBranch = toNativeAscii(branch);
         System.out.println(toJString(t4.init_t4(pId, pPassword, pEmpty)));
         System.out.println(toJString(t4.show_list()));
         System.out.println(toJString(t4.show_ip()));
         System.out.println(toJString(t4.show_version()));
-        System.out.println(toJString(t4.fo_unsettled_qry(toNativeAscii("0000"), toNativeAscii("0004"), toNativeAscii("0000"),
-            toNativeAscii("0000"), toNativeAscii("0000"), toNativeAscii(""), toNativeAscii(branch), toNativeAscii(id), toNativeAscii("1"), toNativeAscii("0"), toNativeAscii("1"))));
-        while(true) {
-            if(t4.check_response_buffer() == 0) {
-                Thread.sleep(1000);
-                continue;
-            }
-            else {
-                System.out.println(toJString(t4.get_response_log()));
-            }
+
+        String ret = toJString(t4.add_acc_ca(pBranch, pAccount, pId, toNativeAscii(libPath), pPassword));
+        System.out.println(ret);
+        // ret = toJString(t4.verify_ca_pass(pBranch, pAccount));
+        // System.out.println(ret);
+        Vector<String> rets = queryUnsettled(pBranch, pAccount);
+        for(String str : rets) {
+            System.out.println(str);
         }
     }
 }
