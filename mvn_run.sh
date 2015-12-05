7e2d5bb1c1f7e49defb13901a6fcb410f9e441b3
yoshi <yoshijava@gmail.com>
2015-12-06 05:26:49 +0800

    fixed scripts.

--- mvn_run.sh
+++ mvn_run.sh
@@ -7,8 +7,8 @@ do
     echo $str
     echo Processing MTX...
     grep -a MTX $f | tr "," " " | awk '{print $1" "$4" "$7}' | sort -n > output/intermediate/${str}_MTX.txt
-    mvn exec:java -Dexec.mainClass=oracle.bband.Oracle -Dexec.args=output/intermediate/${str}_MTX.txt | grep -v INFO | tee output/results/${str}_MTX.txt
+    mvn exec:java -Dexec.mainClass=oracle.Oracle -Dexec.args=output/intermediate/${str}_MTX.txt | grep -v INFO | tee output/results/${str}_MTX.txt
     # echo Processing TX...
 	# grep -a ",TX" $f | tr "," " " | awk '{print $1" "$4" "$7}' | sort -n > output/intermediate/${str}_TX.txt
-    # mvn exec:java -Dexec.mainClass=oracle.bband.Oracle -Dexec.args=output/intermediate/${str}_TX.txt | grep -v INFO | tee output/results/${str}_TX.txt
+    # mvn exec:java -Dexec.mainClass=oracle.Oracle -Dexec.args=output/intermediate/${str}_TX.txt | grep -v INFO | tee output/results/${str}_TX.txt
 done
